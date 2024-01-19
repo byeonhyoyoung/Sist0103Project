7c3d79e51a0af0a54d6461818f948d6c012c7866
hyoyoung <gydud0109@naver.com>
2024-01-19 12:43:25 +0900

    day0119

--- javaproject/SistJavaProject/src/day0118/QuizSwingArrEvent_11.java
+++ javaproject/SistJavaProject/src/day0118/QuizSwingArrEvent_11.java
@@ -3,18 +3,23 @@ package day0118;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class QuizSwingArrEvent_11 extends JFrame implements ActionListener{
 
 	Container cp;
 	JButton [] btn=new JButton[5];
-	String [] btnLabel= {"Pink","Blue","White","Gray","Orange"};
+	JLabel[] btnLabel = new JLabel[1]; // JLabel 배열로 수정
+	//String [] btnLabel= {"Pink","Blue","White","Gray","Orange"};
+	String [] str= {"안녕하세요"};
 	Color [] btncolors= {Color.pink,Color.blue,Color.white,Color.gray,Color.orange};
 	
 	public QuizSwingArrEvent_11(String title) {  
@@ -30,6 +35,16 @@ public class QuizSwingArrEvent_11 extends JFrame implements ActionListener{
 	
 	public void initDesign()
 	{
+		Panel pCenter=new Panel(new GridLayout(3, 3));
+		this.add(pCenter,BorderLayout.CENTER);
+		
+		for(int i=0;i<btnLabel.length;i++)
+		{
+			btnLabel[i] = new JLabel(str[i], JLabel.CENTER);
+            pCenter.add(btnLabel[i]); // 중앙 패널에 JLabel 추가
+			
+		}
+		
 		JPanel panel=new JPanel();
 		panel.setBackground(Color.black);
 		
@@ -38,7 +53,7 @@ public class QuizSwingArrEvent_11 extends JFrame implements ActionListener{
 		for(int i=0;i<btn.length;i++)
 		{
 			//버튼 5개생성
-			btn[i]=new JButton(btnLabel[i]);
+			btn[i] = new JButton(btnLabel[0].getText()); // 버튼에 JLabel의 텍스트 설정
 			//버튼 5개에 각각의 버튼백상변경(btnColors)
 			btn[i].setBackground(btncolors[i]);
 			
@@ -55,11 +70,17 @@ public class QuizSwingArrEvent_11 extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		Object ob=e.getSource();
-		
-		
+
+		//각각의 버튼을 누르면 프레임의 배경색 변경
+		for(int i=0;i<btn.length;i++)
+		{
+			if(ob==btn[i])
+			{
+				//각각의 버튼을 누르면 프레임의 배경색 변경
+				cp.setBackground(btncolors[i]);
+			}
+		}
 	}
-	
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
