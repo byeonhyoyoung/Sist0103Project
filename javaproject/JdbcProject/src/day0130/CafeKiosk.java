c01b740212997feffa206085c1a246e0eeb09a1a
hyoyoung <gydud0109@naver.com>
2024-01-30 17:40:18 +0900

    day0130

---
+++ javaproject/JdbcProject/src/day0130/CafeKiosk.java
@@ -0,0 +1,26 @@
+package day0130;
+
+import java.util.Scanner;
+
+public class CafeKiosk {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CafeKiosk cafeKiosk=new CafeKiosk();
+		Scanner sc=new Scanner(System.in);
+		
+		while(true)
+		{
+			System.out.println("**CAFE ORDER");
+			System.out.println("1.주문하기  2.옵션변경  3.주문취소  4.결제하기  5.끝내기");
+			
+			
+			
+		}
+		
+	}
+
+}
